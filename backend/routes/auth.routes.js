a801c9d215b6c3948b0fc6093ff014da8db36567
jrcryn <jericoryancelestino25@gmail.com>
2025-08-27 00:33:24 +0800

    feat: increase failed login and 2FA attempt thresholds to enhance account security

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/register', register); // ililipat in the future to a separate route for admin job controllers
 
 router.get('/check-auth', verifyAuthToken, checkAuth);
-router.post('/login', loginLimiter, login);
+router.post('/login', loginLimiter, login); 
 router.post('/2fa/generate-2fa-secret', verifyPreAuthToken, generate2FASecret);
 router.post('/2fa/verify-2fa', loginLimiter, verifyPreAuthToken, verify2FA);
 router.post('/logout', logout); 
