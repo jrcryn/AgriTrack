dd81574b3639f09f42b0f3b23804977d7cb9d3a6
jrcryn <jericoryancelestino25@gmail.com>
2025-10-05 14:10:15 +0800

    feat: Enhance account lock handling with user feedback in 2FA and login processes

--- backend/controller/authentication/auth.controller.js
+++ backend/controller/authentication/auth.controller.js
@@ -334,6 +334,10 @@ export const verify2FA = async (req, res) => {
             role = 'HVCS';
         }
 
+        if (user.isLocked) {
+            return res.status(403).json({ success: false, message: 'Account locked. Contact IT support to regain access.' });
+        }
+
         if (!user || !role) {
             return res.status(404).json({ success: false, message: 'User not found.' });
         }
